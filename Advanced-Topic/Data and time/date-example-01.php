bf548fc10160b5bcc572cb843f7ca3175dbf51b7
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-09-18 13:59:09 +0600

    Done

---
+++ Advanced-Topic/Data and time/date-example-01.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Today is ". date("d/m/Y").PHP_EOL;
+echo "Today is ". date("d-m-Y").PHP_EOL;
+echo 'Today is '. date("l").PHP_EOL;
+
+?>
---
+++ Advanced-Topic/Data and time/time-example-01.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "the time is " . date ("h:i:sa").PHP_EOL;
+echo "Now time is: ".date("h:i:sa");
